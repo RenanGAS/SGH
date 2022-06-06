81a01e0f7560bd8ef24b61ab15baa6e1a062b8ca
RenanGAS <renansakaoki@gmail.com>
2022-06-05 21:41:07 -0400

    feat: start the Cliente class

---
+++ Classes/Cliente.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Cliente
+{
+public: // Fazer getters/setters
+    int id;
+    string nome;
+    string cpf;
+    string tel;
+};
