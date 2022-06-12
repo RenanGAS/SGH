4bd0193ecece31f6eaeb15bc7d0f00ec65cd5640
RenanGAS <renansakaoki@gmail.com>
2022-06-11 22:36:05 -0400

    feat: add the Quarto's class

---
+++ Classes/Quarto.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Quarto
+{
+public: // Fazer getters/setters
+    int id;
+    int numero;
+    string especificacao;
+    string status;
+};
