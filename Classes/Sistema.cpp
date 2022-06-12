4a2e006528d67fdf29df0dc92879da0cd3ee937b
RenanGAS <renansakaoki@gmail.com>
2022-06-11 22:40:01 -0400

    feat: add some functions of MgrQuarto to Sistema's class

--- Classes/Sistema.cpp
+++ Classes/Sistema.cpp
@@ -1,5 +1,7 @@
 #include "Sistema.h"
 
+// Cliente
+
 bool Sistema::cadastrarCliente(Cliente &cliente)
 {
   return mgrCliente1->cadastrar(cliente);
@@ -13,4 +15,22 @@ bool Sistema::atualizarCliente(Cliente &cliente)
 Cliente *Sistema::pesquisarCliente(string Nome)
 {
   return mgrCliente1->pesquisar(Nome);
+}
+
+
+// Quarto
+
+bool Sistema::cadastrarQuarto(Quarto &quarto)
+{
+  return mgrQuarto1->cadastrar(quarto);
+}
+
+bool Sistema::atualizarQuarto(Quarto &quarto)
+{
+  return mgrQuarto1->atualizar(quarto);
+}
+
+Quarto *Sistema::pesquisarQuarto(int numero)
+{
+  return mgrQuarto1->pesquisar(numero);
 }
--- Classes/Sistema.h
+++ Classes/Sistema.h
@@ -1,14 +1,20 @@
 #pragma once
 
 #include "../Mgrs/MgrCliente.h"
+#include "../Mgrs/MgrQuarto.h"
 
 class Sistema
 {
   // Criamos managers por questões de responsabilidade
 
   MgrCliente *mgrCliente1 = new MgrCliente();
+  MgrQuarto *mgrQuarto1 = new MgrQuarto();
 public:
   bool cadastrarCliente(Cliente &);
   bool atualizarCliente(Cliente &);
   Cliente *pesquisarCliente(string);
+
+  bool cadastrarQuarto(Quarto &);
+  bool atualizarQuarto(Quarto &);
+  Quarto *pesquisarQuarto(int);
 };
