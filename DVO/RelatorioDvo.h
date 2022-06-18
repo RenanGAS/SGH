93bcdcfb6ded59909a953b925fc980ed8343cac8
RenanGAS <renansakaoki@gmail.com>
2022-06-18 11:41:28 -0400

    feat: add a DVO for Relatorio class, to validate its attributes

---
+++ DVO/RelatorioDvo.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+#define MAX_QUARTOS 10
+
+class RelatorioDvo
+{
+public:
+    bool verificarPeriodo(string dataInicio, string dataFinal);
+};
