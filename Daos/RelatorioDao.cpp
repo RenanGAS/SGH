7324b262d17360e5ccb633d03294b6bb418da28c
RenanGAS <renansakaoki@gmail.com>
2022-06-20 11:48:58 -0400

    feat: implement the RelatorioDao class

--- Daos/RelatorioDao.cpp
+++ Daos/RelatorioDao.cpp
@@ -1,10 +1,66 @@
 #include "RelatorioDao.h"
 
-
-bool RelatorioDao::update(Relatorio &)
+bool RelatorioDao::create(Relatorio &relatorio)
 {
+    if (this->relatoriosCount == MAX_RELATORIOS)
+    {
+        return false;
+    }
+
+    this->relatorios[this->relatoriosCount++] = &relatorio;
+
+    return true;
 }
 
-bool RelatorioDao::delete_(Relatorio &)
+Relatorio *RelatorioDao::retrieve(int relatorioID)
 {
+    for (int i = 0; i < this->relatoriosCount; i++)
+    {
+        if (this->relatorios[i]->getID() == relatorioID)
+        {
+            return this->relatorios[i];
+        }
+    }
+
+    return NULL;
+}
+
+bool RelatorioDao::update(Relatorio &relatorio)
+{
+    Relatorio *relatorio1 = retrieve(relatorio.getID());
+
+    if (relatorio1 == NULL)
+    {
+        return false;
+    }
+
+    relatorio1->setID(relatorio.getID());
+    relatorio1->setDataInicio(relatorio.getDataInicio());
+    relatorio1->setDataFinal(relatorio.getDataFinal());
+
+    return true;
+}
+
+bool RelatorioDao::delete_(Relatorio &relatorio)
+{
+    for (int i = 0; i < MAX_RELATORIOS; i++)
+    {
+        if (this->relatorios[i]->getID() == relatorio.getID())
+        {
+            delete relatorios[i];
+            for (int j = i; j < MAX_RELATORIOS - 1; j++)
+            {
+                this->relatorios[j] = this->relatorios[j + 1];
+            }
+            this->relatoriosCount -= 1;
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
+
+Relatorio **RelatorioDao::list()
+{
+    return this->relatorios;
 }
--- Daos/RelatorioDao.h
+++ Daos/RelatorioDao.h
@@ -4,9 +4,17 @@
 #include <string>
 using namespace std;
 
+#define MAX_RELATORIOS 10
+
 class RelatorioDao
 {
+    Relatorio *relatorios[MAX_RELATORIOS];
+    int relatoriosCount = 0;
+    
 public:
+    bool create(Relatorio &);
+    Relatorio *retrieve(int);
     bool update(Relatorio &);
     bool delete_(Relatorio &);
+    Relatorio **list();
 };
