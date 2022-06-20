2304d1845e0fc088bf374138e22bf09738efae7b
RenanGAS <renansakaoki@gmail.com>
2022-06-20 11:51:50 -0400

    feat: implement the crud functions and some specific ones of MgrRelatorio class

--- Mgrs/MgrRelatorio.cpp
+++ Mgrs/MgrRelatorio.cpp
@@ -1,1 +1,52 @@
 #include "MgrRelatorio.h"
+
+int MgrRelatorio::calcularReservas()
+{
+}
+
+float MgrRelatorio::calcularRenda()
+{
+}
+
+bool MgrRelatorio::cadastrar(Relatorio &relatorio)
+{
+    return this->relatorioDao->create(relatorio);
+}
+
+void MgrRelatorio::listar()
+{
+    for (int i = MAX_RELATORIOS - 1; i >= 0; i--)
+    {
+        Relatorio **relatorios = this->relatorioDao->list();
+
+        if (relatorios[i] != NULL)
+        {
+            cout << relatorios[i]->toStringRelatorio();
+        }
+    }
+}
+
+bool MgrRelatorio::gerar(Relatorio &relatorio)
+{
+    if (!(this->relatorioDvo->verificarPeriodo(relatorio.getDataInicio(), relatorio.getDataFinal())))
+    {
+        return false;
+    }
+
+    relatorio.setQuantidadeReservas(calcularReservas());
+    relatorio.setRenda(calcularRenda());
+
+    return true;
+}
+
+bool MgrRelatorio::atualizar(Relatorio &relatorio)
+{
+    this->relatorioDao->update(relatorio);
+}
+
+bool MgrRelatorio::imprimir(Relatorio &relatorio)
+{
+    relatorio.toStringRelatorio();
+
+    return true;
+}
--- Mgrs/MgrRelatorio.h
+++ Mgrs/MgrRelatorio.h
@@ -2,16 +2,24 @@
 
 #include "../Classes/Relatorio.h"
 #include "../Daos/RelatorioDao.h"
-#include "../DVO/RelatorioDvo.h"
-
+#include "../Dvos/RelatorioDvo.h"
+#include "../Daos/ReservaDao.h"
 
 class MgrRelatorio
 {
     RelatorioDao *relatorioDao = new RelatorioDao();
     RelatorioDvo *relatorioDvo = new RelatorioDvo();
+    ReservaDao *reservaDao1 = new ReservaDao();
+    Reserva **reservas = reservaDao1->list();
+
+    int calcularReservas();
+    float calcularRenda();
 
 public:
-    bool gerar(Relatorio &); // Verifica dados
+    bool cadastrar(Relatorio &);
     bool atualizar(Relatorio &); // Corrige datas
-    bool imprimir(Relatorio &); // Printa informações
+    void listar();
+
+    bool gerar(Relatorio &);     // Verifica dados
+    bool imprimir(Relatorio &);  // Printa informações
 };
