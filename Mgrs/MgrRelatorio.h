4f0e0685fb92725cf5a4df1460a7b5dbbbe0693d
RenanGAS <renansakaoki@gmail.com>
2022-06-21 08:15:32 -0400

    fix: delete an unnecessary include and instantiation

--- Mgrs/MgrRelatorio.h
+++ Mgrs/MgrRelatorio.h
@@ -2,13 +2,11 @@
 
 #include "../Daos/RelatorioDao.h"
 #include "../Dvos/RelatorioDvo.h"
-#include "../Mgrs/MgrReserva.h"
 
 class MgrRelatorio
 {
     RelatorioDao *relatorioDao = new RelatorioDao();
     RelatorioDvo *relatorioDvo = new RelatorioDvo();
-    MgrReserva *mgrReserva1 = new MgrReserva();
     Reserva **reservasValidas = relatorioDvo->getReservasValidas();
     int countReservasValidas = relatorioDvo->getCountReservasV();
 
